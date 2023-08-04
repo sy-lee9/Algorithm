e60908cd6f39d050da7db4395b8bdd45503ab0af
Lee suyeon <131157645+sy-lee9@users.noreply.github.com>
2023-08-05 00:33:42 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int hp) {
         int answer = 0;
-        
+/*        
         int result = hp/5;
         answer += result;
         
@@ -14,6 +14,9 @@ class Solution {
         	result = hp%5%3/1;
             answer += result;     
         }
+*/
+        
+        answer = (hp/5) + ((hp%5) / 3) + ((hp%5) % 3);
         
         return answer;
     }
