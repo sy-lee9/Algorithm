0fec83aa4257654ae9f05995c860c94fc3c83342
Lee suyeon <131157645+sy-lee9@users.noreply.github.com>
2023-08-04 23:11:31 +0900

    [level 0] Title: 세균 증식, Time: 0.03 ms, Memory: 75.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
@@ -1,10 +1,13 @@
 class Solution {
     public int solution(int n, int t) {
         int answer = n;
-        
+/*
         for(int i=0; i<t; i++){
             answer *= 2;
         }
+*/
+        
+        answer = n * (int)Math.pow(2, t);
         
         return answer;
     }
