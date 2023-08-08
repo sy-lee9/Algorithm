0e43d7df2461719e7997dacec810b9b24f6bd613
Lee suyeon <131157645+sy-lee9@users.noreply.github.com>
2023-08-08 22:10:05 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        
+		double dn = (double) num1/num2*1000;
+		answer = (int) dn;
+        
+        return answer;
+    }
+}
