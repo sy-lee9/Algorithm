7d39126463554bef5bb3c3b3959556f5c7b55414
Lee suyeon <131157645+sy-lee9@users.noreply.github.com>
2023-08-11 17:25:55 +0900

    [unrated] Title: 소문자로 바꾸기, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181876. 소문자로 바꾸기/소문자로 바꾸기.java
+++ 프로그래머스/unrated/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -2,11 +2,7 @@ class Solution {
     public String solution(String myString) {
         String answer = "";
 		
-		char[] myChars = myString.toCharArray();
-		
-		for (int i = 0; i < myChars.length; i++) {
-			answer += Character.toString(myChars[i]).toLowerCase();
-		}
+		answer = myString.toLowerCase();
         
         return answer;
     }
