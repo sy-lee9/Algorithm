17a422f99528bedba5d62a423eb5f6ea9d423e2d
Lee suyeon <131157645+sy-lee9@users.noreply.github.com>
2023-08-13 00:28:57 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.37 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        
+        my_string = my_string.toLowerCase();
+        char[] arr = my_string.toCharArray();
+        Arrays.sort(arr);
+        
+        answer = String.valueOf(arr);
+		
+        return answer;
+    }
+}
