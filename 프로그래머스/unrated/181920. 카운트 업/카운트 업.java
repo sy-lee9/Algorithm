8fc0c7da28850c775180ec72d0e5297f714f9c5f
Lee suyeon <131157645+sy-lee9@users.noreply.github.com>
2023-11-02 18:32:19 +0900

    [level unrated] Title: 카운트 업, Time: 0.02 ms, Memory: 67.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -1,10 +1,13 @@
-import java.util.stream.IntStream;
-
 class Solution {
-    public int[] solution(int start, int end) {
+    public int[] solution(int start_num, int end_num) {
         int[] answer = {};
-		
-		answer = IntStream.rangeClosed(start, end).toArray();
+        answer = new int[end_num-start_num+1];
+        int startNum = 0;
+        
+        for(int i=start_num; i<=end_num; i++){
+            answer[startNum] = i;
+            startNum++;
+        }
         
         return answer;
     }
