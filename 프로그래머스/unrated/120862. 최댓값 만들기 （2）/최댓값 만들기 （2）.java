09aedb749d7e15785e44c745fb0ee14c8a1cf990
Lee suyeon <131157645+sy-lee9@users.noreply.github.com>
2023-11-08 16:07:57 +0900

    [level unrated] Title: 최댓값 만들기 (2), Time: 0.45 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        
+        //배열 정렬
+        Arrays.sort(numbers);
+        //index 0 * index1의 값 가져오기
+        int sum1 = numbers[0]*numbers[1];
+        //index-1 * index-2의 값 가져오기
+        int sum2 = numbers[numbers.length-1]*numbers[numbers.length-2];
+        
+        //두 값을 비교해서 큰 값을 answer에 저장
+        if(sum1>sum2){
+            answer = sum1;
+        }else {            
+            answer = sum2;
+        }
+        
+        return answer;
+    }
+}
