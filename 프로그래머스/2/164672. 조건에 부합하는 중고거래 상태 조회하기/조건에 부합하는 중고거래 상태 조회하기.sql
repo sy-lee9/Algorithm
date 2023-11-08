c59c17e5584505e4290b69bf21386c6140db97a2
Lee suyeon <131157645+sy-lee9@users.noreply.github.com>
2023-11-08 20:49:07 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,15 @@
+-- 코드를 입력하세요
+SELECT
+    BOARD_ID
+    ,WRITER_ID
+    ,TITLE
+    ,PRICE
+    ,CASE
+        WHEN STATUS = 'SALE' THEN '판매중' 
+        WHEN STATUS = 'RESERVED' THEN '예약중'  
+        WHEN STATUS = 'DONE' THEN '거래완료'    
+     END
+FROM USED_GOODS_BOARD 
+WHERE 1=1
+AND TO_CHAR(CREATED_DATE,'YYYY-MM-DD') = '2022-10-05'
+ORDER BY BOARD_ID DESC;
