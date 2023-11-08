68785baccbf1b0d5dfc332047065e4cbb280727f
Lee suyeon <131157645+sy-lee9@users.noreply.github.com>
2023-11-08 16:52:01 +0900

    [level unrated] Title: 접두사인지 확인하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -0,0 +1,11 @@
+function solution(my_string, is_prefix) {
+    var answer = 0;
+    
+    if (my_string.startsWith(is_prefix)) {
+        answer = 1;
+    } else {
+        answer = 0;
+    }
+    
+    return answer;
+}
