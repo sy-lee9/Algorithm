ca6a8a30adc378269037dd6916deb5d18bd72892
Lee suyeon <131157645+sy-lee9@users.noreply.github.com>
2023-11-09 21:51:28 +0900

    [level unrated] Title: 꼬리 문자열, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.js
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.js
@@ -4,7 +4,7 @@ function solution(str_list, ex) {
     //배열의 크기만큼 for문 돌기
     for(let i=0; i<str_list.length; i++){
         //배열의 값을 가져와 ex를 포함하지 않으면
-        if(str_list[i].indexOf(ex) == -1){
+        if(!str_list[i].includes(ex)){
             //answer에 저장
             answer += str_list[i];
         }
