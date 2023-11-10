1945d1a0b457fc2e32e90e12496f77c627855099
Lee suyeon <131157645+sy-lee9@users.noreply.github.com>
2023-11-10 15:39:40 +0900

    [level unrated] Title: 주사위 게임 1, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,13 @@
+function solution(a, b) {
+    var answer = 0;
+    
+    if(a%2 != 0 && b%2 != 0){
+        answer = a*a+b*b;
+    }else if(a%2 != 0 || b%2 != 0){
+        answer = 2*(a+b);
+    }else if(a%2 == 0 && b%2 == 0){
+        answer = Math.abs(a-b);
+    }
+    
+    return answer;
+}
