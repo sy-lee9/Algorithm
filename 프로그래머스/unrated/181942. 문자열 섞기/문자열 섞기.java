2ef008780bc6d361a63ce1d402934a06125a9e35
Lee suyeon <131157645+sy-lee9@users.noreply.github.com>
2023-11-15 21:30:45 +0900

    [level unrated] Title: 문자열 섞기, Time: 9.37 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/unrated/181942. 문자열 섞기/문자열 섞기.java
@@ -2,15 +2,11 @@ class Solution {
     public String solution(String str1, String str2) {
         String answer = "";
         
-        //str1과 str2를 배열로 변환
-        String[] arr1 = str1.split("");
-        String[] arr2 = str2.split("");
-        
-        //배열의 크기만큼 for문 돌기
-        for(int i=0; i<arr1.length; i++){
-            //각 list의 i값들을 가져와 answer에 추가
-            answer += arr1[i];
-            answer += arr2[i];
+        //str1의 길이만큼 for문 돌기
+        for(int i=0; i<str1.length(); i++){
+            //각 문자열의 i값들을 가져와 answer에 추가
+            answer += str1.charAt(i);
+            answer += str2.charAt(i);
         //for문 끝
         }
         
