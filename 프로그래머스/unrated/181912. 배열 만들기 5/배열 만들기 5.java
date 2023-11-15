bc331d528156f6e53b4fe0fedefe2de4ab436707
Lee suyeon <131157645+sy-lee9@users.noreply.github.com>
2023-11-16 01:00:19 +0900

    [level unrated] Title: 배열 만들기 5, Time: 9.31 ms, Memory: 93.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.java
+++ 프로그래머스/unrated/181912. 배열 만들기 5/배열 만들기 5.java
@@ -2,23 +2,6 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] intStrs, int k, int s, int l) {
-        
-		List<Integer> numList = new ArrayList<Integer>();
-		
-		List<String> list = Arrays.asList(intStrs);
-		
-		for(int i=0; i<list.size(); i++) {
-			int num = Integer.parseInt(list.get(i).substring(s, s+l));
-			if(num > k) {
-				numList.add(num);
-			}
-		}
-
-		int[] answer = new int[numList.size()];
-		for(int j=0; j<numList.size(); j++) {
-			answer[j] = numList.get(j);
-		}
-        
-        return answer;
+        return Arrays.stream(intStrs).mapToInt(e -> Integer.parseInt(e.substring(s,s+l))).filter(e -> e > k).toArray();
     }
 }
