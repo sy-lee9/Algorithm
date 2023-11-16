8bf63d81335619bbb3074b080d49acffa1403517
Lee suyeon <131157645+sy-lee9@users.noreply.github.com>
2023-11-16 14:05:17 +0900

    [level unrated] Title: 1로 만들기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.js
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.js
@@ -1,17 +1,26 @@
 function solution(num_list) {
     var answer = 0;
     
-    for(let i=0; i<num_list.length; i++){
-        while(num_list[i] > 1){            
-            if(num_list[i]%2 == 0){
-                num_list[i] = num_list[i]/2;
-                answer++;
-            }else if(num_list[i]%2 != 0 && num_list[i] != 1){
-                num_list[i] = (num_list[i]-1)/2;
-                answer++;
+    //배열의 값을 하나씩 꺼내서
+    num_list.forEach((num)=>{
+        //배열의 값이 1보다 클 경우 while문 돌기
+        while(num > 1){
+            //짝수이면
+            if(num%2 == 0){
+                //num_list[i]/2
+                num = num/2
+                //answer++
+                answer++
+            }
+            //num_list[i]가 홀수이면
+            else{
+                //(num_list[i]-1)/2
+                num = (num-1)/2
+                //answer++
+                answer++
             }
         }
-    }
+    })
     
     return answer;
 }
