7a3c6096df243b88a47b97cb48ae87859dd9b1dc
Lee suyeon <131157645+sy-lee9@users.noreply.github.com>
2023-11-20 14:17:08 +0900

    [level unrated] Title: 간단한 논리 연산, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.js
+++ 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.js
@@ -3,19 +3,19 @@ function solution(x1, x2, x3, x4) {
     let sum1 = true;
     let sum2 = true;
     
-    if(x1 == true || x2 == true){
+    if(x1||x2){
         sum1 = true;
     }else {
         sum1 = false;
     }
     
-    if(x3 == true || x4 == true){
+    if(x3||x4){
         sum2 = true;
     }else {
         sum2 = false;
     }
     
-    if(sum1 == false || sum2 == false){
+    if(!sum1||!sum2){
         answer = false;
     }
     
